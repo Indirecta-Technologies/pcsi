e5a8492d4416c79edf8970753105b847452e06d6
Andrew <andreapascullita@gmail.com>
2022-01-31 15:27:35 +0100

    compress cmd

--- src/core/commands/xfs/compress.lua
+++ src/core/commands/xfs/compress.lua
@@ -4,9 +4,9 @@ local cmd = {
 	desc = [[]],
 	usage = [[$ ]],
 	fn = function(plr, pCsi, essentials, args)
-		local oldBytes = xfs:totalBytesInInstance(args[1])
+		local oldBytes = pCsi.xfs:totalBytesInInstance(args[1])
 		pCsi.xfs.compress(args[1])
-		local newBytes = xfs:totalBytesInInstance(args[1])
+		local newBytes = pCsi.xfs:totalBytesInInstance(args[1])
 
 		essentials.Console.info("Compressed "..args[1]..": "..oldBytes.." siB --> "..newBytes.." siB")
 	end,
