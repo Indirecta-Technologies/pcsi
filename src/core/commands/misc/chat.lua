fbd55908bee6b51625b6a147ecf74a768a0835fb
Andrew <andreapascullita@gmail.com>
2022-03-04 20:49:17 +0100

    Add aes256 to chat command
    independent encryption without revealing keys

--- src/core/commands/misc/chat.lua
+++ src/core/commands/misc/chat.lua
@@ -7,8 +7,9 @@ local cmd = {
 		-- THIS DOES NOT FILTER MESSAGES YET!!!!!!!!!!!!!!!!
 
 		-- Comunicate over a bindable in Server Storage, support authentication and encryption peer to peer (first person to start)?
+		pCsi.io.write("Initializing chat..")
 		local MessagingService = game:GetService("MessagingService")
-		local topicname = "_xChat"..game.GameId
+		local topicname = "_xChat" .. game.GameId
 		local remote
 
 		remote = game:GetService("ServerStorage"):WaitForChild("_xChat" .. game.JobId, 3)
@@ -17,8 +18,8 @@ local cmd = {
 
 		local oldparse = pCsi.parseCommand
 
-		local peers = {}
-		local room = pCsi.libs.sha_256().updateStr(game.JobId .. table.concat(args)).finish().asHex()
+		local room = pCsi.libs.sha_256().updateStr(game.JobId .. table.concat(args)).finish()
+		local roomstr = room.asHex()
 
 		local NAME_COLORS = {
 			Color3.new(253 / 255, 41 / 255, 67 / 255), -- BrickColor.new("Bright red").Color,
@@ -52,82 +53,35 @@ local cmd = {
 			return NAME_COLORS[((GetNameValue(pName) + color_offset) % #NAME_COLORS) + 1]
 		end
 
+		local aes256 = pCsi.libs.aes_256
+		local ascii85 = pCsi.libs.ascii85
+		local _key = aes256.schedule256(tonumber("0x" .. roomstr))
 
-		local subscribeSuccess, subscribeConnection = pcall(function()
-			return MessagingService:SubscribeAsync(topicname, function(message)
-			local data = message.Data
-			if not data.header or not data.player then
-				return
-			end
+		local jsoncrypt = {
+			encrypt = function(table, key)
+				table["_timestamp"] = tick()
+				local success, response = pcall(function()
+					table = game:GetService("HttpService"):JSONEncode(table)
+				end)
+				if not success then
+					return nil, response
+				end
+				return ascii85.encode(aes256.ECB(aes256.encrypt, key, table)) or nil
+			end,
+			decrypt = function(table, key)
+				local success, response = pcall(function()
+					table = ascii85.decode(table)
+					table = aes256.ECB(aes256.decrypt, key, table) or nil
+					table = game:GetService("HttpService"):JSONDecode(table)
+				end)
+				if not success then
+					return nil, response
+				end
+				return table
+			end,
+		}
 
-			local displayname = "<b>?</b>"
-			if data.player then 
-				local color = ComputeNameColor(data.player[2])
-				displayname = '<b><font color="rgb('
-				.. math.round(color.R * 255)
-				.. ","
-				.. math.round(color.G * 255)
-				.. ","
-				.. math.round(color.B * 255)
-				.. ')">'
-				.. data.player[2]
-				.. "</font></b>"
-			end
-		
-
-			if data.header == "startSession" and data.room == room then
-				local text = displayname.." joined"
-
-				pCsi.io.write(text)
-			elseif data.header == "endSession" and data.room == room then
-				local text = displayname.." left"
-
-				pCsi.io.write(text)
-
-				peers[data.player.UserId] = nil
-			elseif data.header == "messageSession" and data.room == room and data.message then
-				local text = "("..displayname.."): "
-					.. data.message
-						:gsub("&", "&amp;")
-						:gsub("<", "&lt;")
-						:gsub(">", "&gt;")
-						:gsub('"', "&quot;")
-						:gsub("'", "&apos;")
-				pCsi.io.write(text)
-			end
-			end)
-		end)
-
-
-		local text = "Joined chatroom <b>" .. room:sub(1,18) .. "</b>, use '!q' to leave"
-		pCsi.io.write(text)
-
-		local function startSession(plra)
-			local publishSuccess, publishResult = pcall(function()
-				MessagingService:PublishAsync(topicname, {
-					header = "startSession",
-					room = room,
-					player = {plra.UserId, plra.Name},
-				})
-			end)
-			if not publishSuccess then
-				warn(publishResult)
-			end
-		end
-		local function endSession(plra)
-			local publishSuccess, publishResult = pcall(function()
-				MessagingService:PublishAsync(topicname, {
-					header = "endSession",
-					room = room,
-					player = {plra.UserId, plra.Name},
-				})
-			end)
-			if not publishSuccess then
-				warn(publishResult)
-			end
-		end
-
-		local function processMessage(player, message)
+		local function filtermessage(player, message)
 			local TextService = game:GetService("TextService")
 
 			local textObject
@@ -147,14 +101,106 @@ local cmd = {
 			else
 				message = string.rep("_", #message)
 			end
+			return message
+		end
+
+		local subscribeSuccess, subscribeConnection = pcall(function()
+			return MessagingService:SubscribeAsync(topicname, function(message)
+				local data = message.Data
+				data = jsoncrypt.decrypt(data, _key)
+				if not data or not data.header or not data.player or not data.roomstr == roomstr then
+					return
+				end
+
+				local displayname = "<b>?</b>"
+				if data.player then
+					local color = ComputeNameColor(data.player[2])
+					displayname = '<b><font color="rgb('
+						.. math.round(color.R * 255)
+						.. ","
+						.. math.round(color.G * 255)
+						.. ","
+						.. math.round(color.B * 255)
+						.. ')">'
+						.. data.player[2]
+						.. "</font></b>"
+				end
+
+				if data.header == "startSession" then
+					local text = displayname .. " joined"
+
+					pCsi.io.write(text)
+				elseif data.header == "endSession" then
+					local text = displayname .. " left"
+
+					pCsi.io.write(text)
+
+				elseif data.header == "messageSession" and data.message then
+					data.message = filtermessage(data.player[1], data.message)
+					local text = "("
+						.. displayname
+						.. "): "
+						.. data.message
+							:gsub("&", "&amp;")
+							:gsub("<", "&lt;")
+							:gsub(">", "&gt;")
+							:gsub('"', "&quot;")
+							:gsub("'", "&apos;")
+					pCsi.io.write(text)
+				end
+			end)
+		end)
+
+		essentials.Output:OutputToAll("ClearScreen")
+		local text = "Joined chatroom <b>" .. string.upper(roomstr:sub(1, 16)) .. "</b>, use '!q' to leave"
+		pCsi.io.write(text)
+
+		local function startSession(plra)
+			local publishSuccess, publishResult = pcall(function()
+				MessagingService:PublishAsync(
+					topicname,
+					jsoncrypt.encrypt({
+						header = "startSession",
+						roomstr = roomstr,
+						player = { plra.UserId, plra.Name },
+					}, _key)
+				)
+			end)
+			if not publishSuccess then
+				warn(publishResult)
+			end
+		end
+		local function endSession(plra)
+			local publishSuccess, publishResult = pcall(function()
+				MessagingService:PublishAsync(
+					topicname,
+					jsoncrypt.encrypt({
+						header = "endSession",
+						roomstr = roomstr,
+						player = { plra.UserId, plra.Name },
+					}, _key)
+				)
+			end)
+			if not publishSuccess then
+				warn(publishResult)
+			end
+		end
+
+	
+
+		local function processMessage(player, message)
+			message = filtermessage(player, message)
 
 			local publishSuccess, publishResult = pcall(function()
-				MessagingService:PublishAsync(topicname, {
-					header = "messageSession",
-					room = room,
-					player = {player.UserId, player.Name},
-					message = message,
-				})
+				MessagingService:PublishAsync(
+					topicname,
+					jsoncrypt.encrypt({
+						header = "messageSession",
+						roomstr = roomstr,
+						player = { player.UserId, player.Name },
+						message = message,
+					}, _key)
+				)
 			end)
 			if not publishSuccess then
 				warn(publishResult)
@@ -174,7 +220,9 @@ local cmd = {
 			end
 			if not plra == plr then
 				pCsi.io.write(
-					"This chat session is being used by <b>" .. plr.Name .. "</b>, consider ending the session? '!quit' "
+					"This chat session is being used by <b>"
+						.. plr.Name
+						.. "</b>, consider ending the session? '!quit' "
 				)
 			end
 			input = processMessage(plra, input)
